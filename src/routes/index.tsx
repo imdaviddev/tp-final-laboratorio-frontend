b35807a229159cc6f1d0fd0053f673a04b870a49
pacolu13 <lucavelazquez13@gmail.com>
2024-09-28 22:44:43 -0300

    Cambio de carpetas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,17 @@
-import { Dashboard, Inicio } from '../pages';
+import { Inicio } from '../pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IniciarSesionPage from '../pages/IniciarSesion/IniciarSesionPage';
 import SupervisonHomePage from '../pages/Supervision/SupervisionHomePage';
+import GerenciaHomePage from '../pages/gerencia/gerencia';
 
 const Router = () => {
   return <>
   <BrowserRouter>
     <Routes>
-      <Route  element={<Dashboard/>} path='/' />
-      <Route element={<Inicio />} path="/Inicio" />
+      <Route element={<Inicio />} path="/" />
       <Route element={<IniciarSesionPage />} path="/iniciar-sesion" />
       <Route element={<SupervisonHomePage/>} path="/supervision" />
+      <Route element={<GerenciaHomePage/>} path="/gerencia" />
     </Routes>
   </BrowserRouter>
   </>
