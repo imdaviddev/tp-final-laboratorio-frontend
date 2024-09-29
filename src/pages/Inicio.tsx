48e44a24860900d6e801c2ee9be7742766d41862
pacolu13 <lucavelazquez13@gmail.com>
2024-09-28 22:25:53 -0300

    Pestaña gerencia

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,9 +1,7 @@
-import { Header } from "../components"; //Exportar para ser mas sencillo de entender de donde proviene
 import Main from "../components/Main";
 
 const Inicio = () => {
   return <>
-    <Header/>
     <Main/>
   </>
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,8 @@
 import { Dashboard, Inicio } from '../pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-<<<<<<< Updated upstream
-=======
-import IniciarSesionPage from '../pages/IniciarSesion/IniciarSesionPage';
+//import IniciarSesionPage from '../pages/IniciarSesion/IniciarSesionPage';
 import SupervisonHomePage from '../pages/Supervision/SupervisionHomePage';
 import GerenciaHomePage from '../pages/gerencia/gerencia';
->>>>>>> Stashed changes
 
 const Router = () => {
   return <>
@@ -13,13 +10,9 @@ const Router = () => {
     <Routes>
       <Route  element={<Dashboard/>} path='/' />
       <Route element={<Inicio />} path="/Inicio" />
-<<<<<<< Updated upstream
-
-=======
-      <Route element={<IniciarSesionPage />} path="/iniciar-sesion" />
+      {/*<Route element={<IniciarSesionPage />} path="/iniciar-sesion" />*/}
       <Route element={<SupervisonHomePage/>} path="/supervision" />
       <Route element={<GerenciaHomePage/>} path="/gerencia" />
->>>>>>> Stashed changes
     </Routes>
   </BrowserRouter>
   </>
