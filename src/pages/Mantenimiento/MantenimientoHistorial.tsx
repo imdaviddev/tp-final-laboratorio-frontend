7460a29eefb77d5e16e04e8d25dab32e2a1fb84b
David Cañete <84222921+imdaviddev@users.noreply.github.com>
2024-09-30 20:57:19 -0300

    Fix (#12)
    * Diseño Inicio
    
    * diseño ingresos
    
    * diseño reparaciones
    
    * diseño inicial reparaciones
    
    * fix

--- src/pages/Mantenimiento/MantenimientoHistorial.tsx
+++ src/pages/Mantenimiento/MantenimientoHistorial.tsx
@@ -12,8 +12,8 @@ const MantenimientoHistorial = () => {
             <ColumnaGeneral style={{width: '150px', backgroundColor: 'white', boxShadow: 'none'}}>
               <img src={Camion} style={{width: '100&', height: '100%'}} />
             </ColumnaGeneral>
-            <BoxGeneral style={{flex: '1', justifyContent:'center'}}>
-              <TituloGeneral style={{fontSize: '1.5rem'}}>
+            <BoxGeneral style={{flex: '1', display: 'flex'}}>
+              <TituloGeneral style={{fontSize: '1.5rem', textAlign:'center'}}>
                   Vehiculo #C4222
               </TituloGeneral>
               <BoxGeneral>
