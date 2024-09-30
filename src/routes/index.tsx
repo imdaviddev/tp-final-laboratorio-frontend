c659890c877143f29ef999463c7511af7d980492
David Cañete <daviddanielc25@gmail.com>
2024-09-29 22:59:48 -0300

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import { Inicio } from '../pages';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IniciarSesionPage from '../pages/IniciarSesion/IniciarSesionPage';
-import { SupervisionPageLayout, SupervisionHomePage, CrearRecorridoPage } from '../pages/Supervision';
-import ConductoresPage from '../pages/Supervision/Conductores/Conductores';
 import GerenciaHomePage from '../pages/gerencia/gerencia';
 import ReportesGerencia from '../pages/reportesGerencia/ReportesGerencia';
 import ControlesGerencia from '../pages/controlesGerencia/ControlesGerencia';
+import { CrearRecorridoPage, SupervisionHomePage, SupervisionPageLayout } from '../pages/Supervision';
+import ConductoresPage from '../pages/Supervision/Conductores/Conductores';
 
 const Router = () => {
   return <>
@@ -13,14 +13,14 @@ const Router = () => {
     <Routes>
       <Route element={<Inicio />} path="/" />
       <Route element={<IniciarSesionPage />} path="/iniciar-sesion" />
-      <Route path="/supervision" element={<SupervisionPageLayout />}>
-        <Route index element={<SupervisionHomePage />} />
-        <Route path="crear-recorrido" element={<CrearRecorridoPage />} />
-        <Route path="vehiculos" element={<ConductoresPage />} />
-      </Route>
       <Route element={<GerenciaHomePage/>} path="/gerencia" />
       <Route element={<ReportesGerencia/>} path="/gerencia/reportes" />
       <Route element={<ControlesGerencia/>} path="/gerencia/controles" />
+      <Route element={<SupervisionPageLayout/>} path='/supervision'>
+        <Route index element={<SupervisionHomePage/>} />
+        <Route element={<CrearRecorridoPage/>} path='crear-recorrido'/>  
+        <Route element={<ConductoresPage/>} path='conductores'/>  
+      </Route>      
     </Routes>
   </BrowserRouter>
   </>
