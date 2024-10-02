eb415f3203cafb13b77c49ce9dbf4b35ba8cf6e7
David Cañete <daviddanielc25@gmail.com>
2024-10-02 15:42:51 -0300

    Enrutamiento corregido

--- src/components/HeaderComponent/Header.tsx
+++ src/components/HeaderComponent/Header.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import './header.css'
 import type { SVGProps } from 'react';
 import logoAmazona from '../../../public/LogoAmazona.png'
+import { Link } from "react-router-dom";
+
+const conductores = "http://localhost:5173/conductor/";
+const clientes = "http://localhost:5173/";
+const administracion = "http://localhost:5173/administracion/";
+const mantenimiento = "http://localhost:5173/mantenimiento/";
+const supervision = "http://localhost:5173/supervision/";
+const gerencia = "http://localhost:5173/gerencia/";
 
 export function MdiAccountCircleOutline(props: SVGProps<SVGSVGElement>) {
   return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="#c92222" d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2M7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.5.88 4.93 1.78A7.9 7.9 0 0 1 12 20c-1.86 0-3.57-.64-4.93-1.72m11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33A7.93 7.93 0 0 1 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.5-1.64 4.83M12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6m0 5a1.5 1.5 0 0 1-1.5-1.5A1.5 1.5 0 0 1 12 8a1.5 1.5 0 0 1 1.5 1.5A1.5 1.5 0 0 1 12 11"></path></svg>);
@@ -11,7 +19,7 @@ const NavbarCliente = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Seguimiento</li>
+        <Link to={clientes+""}>Seguimiento</Link>
         <li>Sobre Nosotros</li>
       </ul>
     </nav>
@@ -22,9 +30,9 @@ const NavBarConductor = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Inicio</li>
-        <li>Tus recorridos</li>
-        <li>Reportes</li>
+        <Link to={conductores+""}>Inicio</Link>
+        <Link to={conductores+"recorridos"}>Tus recorridos</Link>
+        <Link to={conductores+"reportes"}>Reportes</Link>
       </ul>
     </nav>
 
@@ -36,10 +44,10 @@ const NavbarSupervisor = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Inicio</li>
-        <li>Crear Recorrido</li>
-        <li>Conductores</li>
-        <li>Reportes</li>
+        <Link to={supervision+""}>Inicio</Link>
+        <Link to={supervision+"crear-recorrido"}>Crear Recorrido</Link>
+        <Link to={supervision+"conductores"}>Conductores</Link>
+        <Link to={supervision+"reportes"}>Reportes</Link>
       </ul>
     </nav>
 
@@ -50,9 +58,9 @@ const NavbarGerencia = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Inicio</li>
-        <li>Estadisticas</li>
-        <li>Controles</li>
+        <Link to={gerencia + ""}>Inicio</Link>
+        <Link to={gerencia + "reportes"}>Estadisticas</Link>
+        <Link to={gerencia + "controles"}>Controles</Link>
       </ul>
     </nav>
 
@@ -63,10 +71,10 @@ const NavbarMantenimiento = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Inicio</li>
-        <li>Ingresos</li>
-        <li>Reparaciones</li>
-        <li>Historial de Tickets</li>
+        <Link to={mantenimiento+ ""}>Inicio</Link>
+        <Link to={mantenimiento+ "ingresos"}>Ingresos</Link>
+        <Link to={mantenimiento+ "reparaciones"}>Reparaciones</Link>
+        <Link to={mantenimiento+ "historial-de-tickets"}>Historial de Tickets</Link>
       </ul>
     </nav>
 
@@ -77,10 +85,10 @@ const NavbarAdministracion = () => {
   return (
     <nav className="header-bottom">
       <ul>
-        <li>Inicio</li>
-        <li>Repuestos</li>
-        <li>Proveedores</li>
-        <li>Reportes</li>
+        <Link to={administracion+ ""}>Inicio</Link>
+        <Link to={administracion+ "repuestos"}>Repuestos</Link>
+        <Link to={administracion+ "proveedores"}>Proveedores</Link>
+        <Link to={administracion+ "reportes"}>Reportes</Link>
       </ul>
     </nav>
 
