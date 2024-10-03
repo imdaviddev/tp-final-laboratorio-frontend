bc15032a2aafc2944bbdc818a7bf539899759cb8
David Cañete <daviddanielc25@gmail.com>
2024-10-03 14:15:18 -0300

    variables de entorno

--- src/components/HeaderComponent/Header.tsx
+++ src/components/HeaderComponent/Header.tsx
@@ -4,15 +4,15 @@ import type { SVGProps } from 'react';
 import logoAmazona from '../../../public/LogoAmazona.png'
 import { Link } from "react-router-dom";
 
+const apiURL: string = import.meta.env.VITE_API_URL;
 
 
-
-const conductores = `http://amazona-logist.netlify.app/conductor/`;
-const clientes = `https://amazona-logist.netlify.app/`;
-const administracion = `https://amazona-logist.netlify.app/administracion/`;
-const mantenimiento = `https://amazona-logist.netlify.app/mantenimiento/`;
-const supervision = `https://amazona-logist.netlify.app/supervision/`;
-const gerencia = `https://amazona-logist.netlify.app/gerencia/`;
+const conductores = `${apiURL}/conductor/`;
+const clientes = `${apiURL}/`;
+const administracion = `${apiURL}/administracion/`;
+const mantenimiento = `${apiURL}/mantenimiento/`;
+const supervision = `${apiURL}/supervision/`;
+const gerencia = `${apiURL}/gerencia/`;
 
 export function MdiAccountCircleOutline(props: SVGProps<SVGSVGElement>) {
   return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="#c92222" d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2M7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.5.88 4.93 1.78A7.9 7.9 0 0 1 12 20c-1.86 0-3.57-.64-4.93-1.72m11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33A7.93 7.93 0 0 1 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.5-1.64 4.83M12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6m0 5a1.5 1.5 0 0 1-1.5-1.5A1.5 1.5 0 0 1 12 8a1.5 1.5 0 0 1 1.5 1.5A1.5 1.5 0 0 1 12 11"></path></svg>);
