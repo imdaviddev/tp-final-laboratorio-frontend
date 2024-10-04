1ffbcfe89be75372203e0bff3697d447de4da1a9
pacolu13 <lucavelazquez13@gmail.com>
2024-10-04 20:10:20 -0300

    Styled para la pestaña conductor

--- src/pages/Conductor/ComponentsUI/index.tsx
+++ src/pages/Conductor/ComponentsUI/index.tsx
@@ -1,32 +1,114 @@
 import { styled } from "styled-components"
 
 
+export const MensajeBienvenida = styled.div`
+  margin-top: 35px;
+  margin-bottom: 35px;
+  padding: 20px;
+  width: 450px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  font-weight: bold;
+  color: white;
+  background-color: red;
+  border-radius: 10px;
+  box-shadow: 3px 3px 3px gray;
+  text-align: center;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+
+  /* Estilos para pantallas pequeñas (móviles) */
+  @media (max-width: 600px) {
+    width: 250px;
+    font-size: 18px;
+  }
+
+`
+
+export const DescHijo = styled.div`
+  margin-bottom: 25px;
+  padding: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  color: red;
+  background-color: #f9f9f9;
+  border: 1px solid #C90035; /* Bordecito bonito con el terciario */
+  border-radius: 8px;
+  text-align: center;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+  max-width: 600px; /* Para que no se extienda demasiado en pantallas grandes */
+`
+
+//Centra todo lo disponible de la pagina ¿Se puede mejorar?
+export const ContainerPadre = styled.div`
+    margin-top: 125px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+//Contiene bordes de sombra y los datos en si del recorrido
+export const ContainerHijo = styled.div`
+    height: 350px;
+    width: 450px;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 25px;
+    box-shadow: 5px 5px 5px 6px gray;
+
+    @media (max-width: 600px) {
+        width: 350px
+    }
+`
+//Centra El detalle e imagen del recorrido
+export const ContainerDetalle = styled.div`
+    width: 350px;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
 export const Button = styled.button`
-padding: 15px;
-margin: 10px;
-color: white;
-background-color: red;
-border-style: none;
-border-radius: 15px;
-box-shadow: 5px 5px 5px gray;
+    padding: 15px;
+    margin: 10px;
+    color: white;
+    background-color: red;
+    border-style: none;
+    border-radius: 15px;
+    box-shadow: 5px 5px 5px gray;
 `
 
 export const RecorridoDetalle = styled.div`
-margin: 15px;
-width: 350px;
-display: flex;
-justify-content: space-around;
-align-items: center;
-border-radius: 10px;
-box-shadow: 5px 5px 5px 5px gray;
+    margin: 15px;
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 10px;
 `
+//Estilo para los reportes, podria verse de otra forma
+export const Reportes = styled.div`
+    margin-top: 25px;
+    display: flex;
+    align-items: center;
+    height: 200px;
+    width: 450px;
 
-export const RecorridoDetalleContainer = styled.div`
-margin: 15px;
-width: 350px;
-display: flex;
-justify-content: space-around;
-align-items: center;
-border-radius: 10px;
-box-shadow: 5px 5px 5px 5px gray;
+    @media (max-width: 600px) {
+        width: 350px
+    }
 `
--- src/pages/Conductor/ConductorRecorrido.tsx
+++ src/pages/Conductor/ConductorRecorrido.tsx
@@ -1,42 +1,36 @@
 import Header from '../../components/HeaderComponent/Header';
-import { Button, RecorridoDetalle } from '../Conductor/ComponentsUI/index';
+import { RecorridoDetalle, MensajeBienvenida, ContainerPadre, ContainerHijo, DescHijo, Button } from './ComponentsUI';
+import Detalle from './Components/Detalle';
 import './conductor.css'
 
 
 const CoductorRecorrido = () => {
     return <div className='page-container'>
         <Header rol='Conductor' />
-        <div className="wrapper-mensaje-bienvenida">
-            <h1>Tus recorridos</h1>
-        </div>
-        <div className='container-conductor'>
-            <div>
-                <Button>Reporte Semanal</Button>
-                <Button>Reporte Semanal</Button>
-            </div>
-            <br />
-            <br />
-            <p><b>Reporte de rutas</b></p>
+        <ContainerPadre>
 
-            <div className='container-recorrido-conductor'>
+            <MensajeBienvenida>Tus recorridos</MensajeBienvenida>
 
+            <ContainerHijo>
+                <DescHijo>Reporte de rutas</DescHijo>
                 <RecorridoDetalle>
-                    <ul>
-                        <li>Recorrido: #012</li>
-                        <li>Fecha: 23/08/2024</li>
-                    </ul>
+                    <Detalle tipo="Recorrido" valor="#064"></Detalle>
+                    <Detalle tipo="Fecha" valor="25/08/2024"></Detalle>
                     <a href="#">ver detalle</a>
                 </RecorridoDetalle>
                 <RecorridoDetalle>
-                    <ul>
-                        <li>Recorrido: #059</li>
-                        <li>Fecha: 20/08/2024</li>
-                    </ul>
+                    <Detalle tipo="Recorrido" valor="#059"></Detalle>
+                    <Detalle tipo="Fecha" valor="20/08/2024"></Detalle>
                     <a href="#">ver detalle</a>
                 </RecorridoDetalle>
 
-            </div>
-        </div>
+                <div>
+                    <Button>Reporte Semanal</Button>
+                    <Button>Reporte Semanal</Button>
+                </div>
+
+            </ContainerHijo>
+        </ContainerPadre>
     </div>
 
 }
