06e22f87a8798c02cc397c915423129449484a50
pacolu13 <lucavelazquez13@gmail.com>
2024-10-06 18:05:31 -0300

    GerenciaInicio
    Solo cambie el nombre para que no cause mas errores

--- src/pages/gerencia/index.tsx
+++ src/pages/gerencia/index.tsx
@@ -1,4 +1,4 @@
-import GerenciaHomePage from './Gerencia';
+import GerenciaHomePage from './GerenciaInicio';
 import GerenciaControles from './GerenciaControles'
 import GerenciaReportes from './GerenciaReportes'
 import GerenciaPageLayout from './GerenciaPageLayout';
